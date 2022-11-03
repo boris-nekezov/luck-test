5ec88d1ef058784234002a89fdbba09d94eeb918
b.nekezov <b.nekezov@godeltech.com>
2022-11-03 17:56:44 +0200

    Set up basic routing

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import axios from 'axios';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from 'react-router-dom';
 
 import { useAppSelector } from './app/hooks';
 import { selectLogin, selectPassword } from './features/user/userSlice';
 import { Button, ColorsIcon } from '@luck-test/ui-kit';
+
+import HomePage from './pages/Home';
 
 import './App.scss';
 
@@ -23,11 +30,16 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <ColorsIcon />
-      <Button label="test" onClick={onRequest} />
-      Hello World!!!
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <ColorsIcon />
+        <Button label="test" onClick={onRequest} />
+        Hello World!!!
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
